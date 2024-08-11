e6c47a7affb1a6484730807d5e5a90851d555ed1
stefegg <stephenegbert@gmail.com>
2024-08-11 18:59:47 -0400

    controllers

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import posts from "./routes/posts.js";
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
+
+const port = process.env.PORT || 8000;
 
 const app = express();
 
-// setup static folder
-app.use(express.static(path.join(__dirname, "public")));
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+//Logger middleware
+app.use(logger);
+// setup static folder
+// app.use(express.static(path.join(__dirname, "public")));
+
+//Routes
+app.use("/api/posts", posts);
+
+//Error handler
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
